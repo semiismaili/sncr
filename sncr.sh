534fccef40e6b052035dbabfa95d1fb27298db0e
semiismaili <semi-97@hotmail.com>
2020-05-26 00:33:58 +0200

    Log functionality and Last Sync Information Implemented

--- sncr.sh
+++ sncr.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+##################################### USAGE #####################################
+usage="Usage
+    sncr (Will run the sync with the provided configuration, if no config is provided it will prompt you for the inital configuration.)
+    sncr -config (Will bring the configuration prompt where you can change parameters)
+        >Changeable parameters:
+        >server address (ex. 192.168.X.X)
+        >server username (the host machine username)
+        how often the sync happens (in minutes, 0 for no automaatic syncing)
+        local directorium (where the files will be synced into, requires absoulute path, default is $HOME/sncr_files)
+        
+    sncr -log (outputs the content of the last logfile)
+    sncr -log -n <number> (outpots only the last <number> lines of the last logfile)
+    
+    sncr -last (outputs when the last successfull sync happened)
+    
+    sncr -help (outputs this usage screen)" 
+##################################################################################
 hiddenfolder=$HOME/.sncr
 config () {
     read -p "Server IP or DNS: " serverip
@@ -44,6 +62,38 @@ if [ "$1" = "-config" ]
 then
     config
     exit 1
+elif [ "$1" = "-log" ]
+then
+    last_modified_file=`find $hiddenfolder/rsynclogs/ -type f -printf "%T@\0%p\0" | awk '
+    {
+        if ($0>max) {
+            max=$0; 
+            getline mostrecent
+        } else 
+            getline
+    } 
+    END{print mostrecent}' RS='\0'`
+    if [ "$2" = "-n" ]
+    then
+        regex='^[0-9]+$' #is a number regex
+        if [[ "$3" =~ $regex ]]
+        then
+            tail $last_modified_file -n $3
+        else
+            echo "sncr: Invalid usage, argument after -n shoud be an number, run -h!"
+        fi
+    elif [ "$2" = "-last" ]
+    then
+        cat $hiddenfolder/last_sync_success.log
+    else
+        cat $last_modified_file
+    fi
+elif [ "$1" = "-last" ]
+    then
+        cat $hiddenfolder/last_sync_success.log
+elif [ "$1" = "-h" ]
+    then
+        echo $usage               
 elif [ $# -eq 0 ] 
     then #actual work
     if [ ! -f $hiddenfolder/sncr.conf ] || [ ! -s $hiddenfolder/sncr.conf ] #if conf file doesnt exist or is empty
@@ -56,13 +106,18 @@ elif [ $# -eq 0 ]
     local_dir=$LOCAL_DIR
     while true
     do
-        rsync -avzP --log-file=$hiddenfolder/rsynclogs/rsync-backup-log-$(date +"%Y-%m-%d").log --delete rsync://$address:12000/files $local_dir
+        rsync -avzP --log-file=$hiddenfolder/rsynclogs/rsync-backup-log-$(date +"%Y-%m-%d").log --delete rsync://$address:12000/files $local_dir 
+        if [ "$?" -eq "0" ]
+        then
+            echo 'Last successfull sync on' $(date) > $hiddenfolder/last_sync_success.log
+        fi
         if [[ $minutes -eq 0 ]]
         then
             break
         fi
         #sleep $minutes
     done
-    #TODO make config file so koj server, koja datoteka, i last synced etc (-p bar)
     #TODO bashrc
+else
+       echo "sncr: Invalid usage, run -h for usage info!"
 fi
