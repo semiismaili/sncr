f639f19565989cf98b5b5026cd18992ffa7cd794
semiismaili <semi-97@hotmail.com>
2020-05-28 02:28:03 +0200

    Implemented -install and -help

--- sncr.sh
+++ sncr.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 ##################################### USAGE #####################################
-usage="Usage
-    sncr (Will run the sync with the provided configuration, if no config is provided it will prompt you for the inital configuration.)
-    sncr -config (Will bring the configuration prompt where you can change parameters)
-        >Changeable parameters:
-        >server address (ex. 192.168.X.X)
-        >server username (the host machine username)
-        how often the sync happens (in minutes, 0 for no automaatic syncing)
-        local directorium (where the files will be synced into, requires absoulute path, default is $HOME/sncr_files)
-        
-    sncr -log (outputs the content of the last logfile)
-    sncr -log -n <number> (outpots only the last <number> lines of the last logfile)
-    
-    sncr -last (outputs when the last successfull sync happened)
-    
-    sncr -help (outputs this usage screen)" 
+usage="Usage:\n
+    sncr (Will run the sync with the provided configuration, if no config is provided it will prompt you for the inital configuration.) \n
+    sncr -config (Will bring the configuration prompt where you can change parameters) \n
+        \n\t Changeable parameters:
+        \n\t server address (ex. 192.168.X.X)
+        \n\t server username (the host machine username)
+        \n\t how often the sync happens (in minutes, 0 for no automaatic syncing)
+        \n\t local directory (where the files will be synced into, requires absoulute path, default is $HOME/sncr_files)
+        \n\n
+    sncr -log (outputs the content of the last logfile) \n
+    sncr -log -n <number> (outpots only the last <number> lines of the last logfile) \n
+    \n
+    sncr -last (outputs when the last successfull sync happened) \n
+
+    sncr -install (moves the sncr script to /usr/bin and deletes the cloned sncr folder cloned from git) \n
+
+    sncr -help (outputs this usage screen)"    
 ##################################################################################
 hiddenfolder=$HOME/.sncr
 config () {
@@ -91,9 +93,22 @@ then
 elif [ "$1" = "-last" ]
     then
         cat $hiddenfolder/last_sync_success.log
-elif [ "$1" = "-h" ]
+elif [ "$1" = "-h" ] || [ "$1" = "-help" ]
     then
-        echo $usage               
+        echo -e $usage
+elif [ "$1" = "-install" ]
+    then
+        echo "Checking if sncr is already installed.."
+        which sncr
+        if [ "$?" -ne "0" ]
+        then
+            sudo cp sncr.sh /usr/bin/sncr
+            cd ..
+            rm -rf sncr
+            echo "Successfully installed sncr to /usr/bin/sncr"
+        else
+            echo "sncr is already installed"
+        fi
 elif [ $# -eq 0 ] 
     then #actual work
     if [ ! -f $hiddenfolder/sncr.conf ] || [ ! -s $hiddenfolder/sncr.conf ] #if conf file doesnt exist or is empty
